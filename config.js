16b573a8b9887c882c99de6c773e2c3a9f7ea0ff
Grezzx <denias.7a@gmail.com>
2025-01-21 01:54:13 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// BOT TELE
+module.exports = {
+  BOT_TOKEN: "8006933155:AAEZ-SpXCksd0Xy2APli5WT5FpWyd0NJnQs", // Token bot Telegram
+  OWNER_ID: "1891979555", // ID pemilik bot
+  allowedGroupIds: [-1002401306074, -1002361556670, -1002386380830, -1002415460020, -4514397992], // ID grup yang diizinkan
+};
